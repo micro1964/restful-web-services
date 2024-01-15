b7612b54cce57c835f41a80a884d407a82115f65
Reggy Williams <reggy_williams@hotmail.com>
2024-01-15 00:35:26 +0000

    CreateUser, DeleteUser and ExceptionHandling

--- src/main/java/com/reggy/rest/webservices/restfulwebservices/user/UserController.java
+++ src/main/java/com/reggy/rest/webservices/restfulwebservices/user/UserController.java
@@ -1,13 +1,17 @@
 package com.reggy.rest.webservices.restfulwebservices.user;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserController {
@@ -22,11 +26,30 @@ public class UserController {
 	
 	@GetMapping("/users/{id}")
 	public User retrieveUser(@PathVariable int id) {
-		return service.findOne(id);
+		User user = service.findOne(id);
+		if(user == null) {
+			throw new UserNotFoundException("id: "+id);
+		}
+		return user;
+	}
+	
+	@DeleteMapping("/users/{id}")
+	public void deleteUser(@PathVariable int id) {
+		User user = service.deleteUserById(id);
+		if(user == null) {
+			throw new UserNotFoundException("id: "+id);
+		}
 	}
 	
 	@PostMapping("/users")
-	public void createUser(@RequestBody User user) {
+	public ResponseEntity<Object> createUser(@RequestBody User user) {
 		User savedUser = service.save(user);
+		
+		URI location = ServletUriComponentsBuilder.
+		fromCurrentRequest().
+		path("/{id}").
+		buildAndExpand(savedUser.getId()).toUri();
+		
+		return ResponseEntity.created(location).build();
 	}
 }
